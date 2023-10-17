9c2eca63810f5d6514cef67155dd509897dcacb2
evgeny <ekarpovsky@gmail.com>
2023-10-17 06:42:27 +0000

    Sync the package infrastructure

--- src/ErrorHandler.ts
+++ src/ErrorHandler.ts
@@ -1,37 +1,48 @@
-import { NextFunction, Request, Response } from "express";
+import { Response } from "express";
 
 import { AppError, ResponseCode } from "./AppError";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    appErrorHandler(err, res);
+export const errorHandler = (error: Error, response?: Response) => {
+  if (response) {
+    if (error instanceof AppError) {
+      appErrorHandler(error, response);
+    } else {
+      internalErrorHandler(error, response);
+    };
   } else {
-    internalErrorHandler(err, res);
+    criticalErrorHandler(error);
   };
 };
 
-const appErrorHandler = (err: AppError, res: Response) => {
-  res.status(err.code).json({ message: err.message });
+const appErrorHandler = (error: AppError, response: Response) => {
+  response.status(error.code).json({ message: error.message });
 };
 
-const internalErrorHandler = (err: Error, res: Response) => {
-  res.status(ResponseCode.INTERNAL_SERVER_ERROR).send('Internal server error');
+const internalErrorHandler = (error: Error, response: Response) => {
+  console.log(`internalErrorHandler ${error.message}`);
+  response.status(ResponseCode.INTERNAL_SERVER_ERROR).send('Internal server error');
 };
 
-const isAppError = (err: Error): boolean => {
-  return (err instanceof AppError);
+const criticalErrorHandler = (error: Error) => {
+  console.log(`criticalErrorHandler ${error.message}`);
+  process.exit(1);
+};
+
+const isAppError = (error: Error): boolean => {
+  return (error instanceof AppError);
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-process.on('unhandledRejection', (err: Error, prom: Promise<unknown>) => {
-  throw err;
+process.on('unhandledRejection', (error: Error) => {
+  throw error;
 });
 
-process.on('uncaughtException', (err: Error) => {
+process.on('uncaughtException', (error: Error) => {
   // just received an error that was never handled, 
   // time to handle it and then decide whether a restart is needed
-  if (!isAppError(err)) {
-    process.exit(1);
-  }
+  if (!isAppError(error)) {
+    criticalErrorHandler(error);
+  };
+  errorHandler(error);
 });
